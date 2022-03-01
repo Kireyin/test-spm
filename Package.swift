a537c1d5b8c460bbfe59fe3c7c940b7582d7c77b
Kireyin <kireyin@icloud.com>
2022-03-01 15:46:17 +0100

    dep

--- Package.swift
+++ Package.swift
@@ -50,7 +50,7 @@ let package = Package(
                     .target(name: "Multiparty"),
                     .target(name: "SCWormhole"),
                     "AFNetworking",
-                    "libPhoneNumberiOS"
+                    "libPhoneNumber"
                 ],
                path: "LSUniversalSDKWrapper"),
         .target(name: "MediaCaptureWrapper",
